180f796ad677919c62a15c448350dbec06cb0640
BERNARDOBOJALIL <bernardo.bojalil.lorenzini@gmail.com>
2025-01-27 12:42:57 -0600

    Arquitectura

---
+++ AgriConnect-Bernardo.Mobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AgriConnect_Bernardo.Mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AgriConnect-Bernardo.Mobile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AgriConnect_Bernardo.Mobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
